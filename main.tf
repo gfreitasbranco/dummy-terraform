c0f2dcb8cdc74611601995cf25dba6b909940c5c
gfreitasbranco <89025141+gfreitasbranco@users.noreply.github.com>
2021-08-26 15:43:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "linux1" {
   ami = data.aws_ami.aws-linux.id
   instance_type = "t2.micro"
   subnet_id = "subnet-0bc6c4486b6c98753"
-  vpc_security_group_ids = "sg-0b5bbaa2b04604e27"
+  vpc_security_group_ids = ["sg-0b5bbaa2b04604e27"]
   tags = {
     Name = "Test Linux Gabriel"
   }
