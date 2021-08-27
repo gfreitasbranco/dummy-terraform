f2afb8f7ff8babe31aa9dab9179a80be9c422c2e
gfreitasbranco <89025141+gfreitasbranco@users.noreply.github.com>
2021-08-27 12:56:40 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-west-2"
 }
 
 # AMI
@@ -34,8 +34,8 @@ data "aws_ami" "aws-linux" {
 resource "aws_instance" "linux1" {
   ami = data.aws_ami.aws-linux.id
   instance_type = "t2.micro"
-  subnet_id = "subnet-0bc6c4486b6c98753"
-  vpc_security_group_ids = ["sg-0b5bbaa2b04604e27"]
+  subnet_id = "subnet-0adba1ecf2f2867e7"
+  vpc_security_group_ids = ["sg-0faa58047af970f59"]
   tags = {
     Name = "Test Linux Gabriel"
   }
